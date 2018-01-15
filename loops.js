fd4e561851b84065e5e137b961c4a8a1681df081
Karen O <k.ortiz026@gmail.com>
2018-01-15 01:09:15 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(array){
+  //Run code 25 times.
+  for(let i=0; i<25;i++){
+    //check if i = 1, if so you have to add a different string
+    if (i===1){
+      array.push(`I am ${i} strange loop.`)
+    }else{
+      array.push(`I am ${i} strange loops.`);  
+    }
+  }
+  //Return array after adding strings.
+  return array;
+}
+
+function whileLoop(number){
+  while(number>0){
+    console.log(--number);
+  }
+  return 'done'
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  do{
+    array.pop();
+  }while(array.length>0 && maybeTrue());
+  return array;
+}

2a102dd1cf990102b7a0eb098219e1e269ac5c4a
Karen O <k.ortiz026@gmail.com>
2018-01-15 00:31:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
 function forLoop(array){
   for(let i=0; i<25;i++){
-    array.push(`I am ${i} strange loop.`);
+    //check if i = 1, if so you have to add a different string
+    if (i===0){
+      array.push(`I am ${i} strange loop.`)
+    }else{
+      array.push(`I am ${i} strange loops.`);  
+    }
   }
 }

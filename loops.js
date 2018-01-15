af878d4b39d3abc2cc081127e680e1df9a7a9a1b
Karen O <k.ortiz026@gmail.com>
2018-01-15 00:40:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,5 @@
 function forLoop(array){
+  //Run code 25 times.
   for(let i=0; i<25;i++){
     //check if i = 1, if so you have to add a different string
     if (i===1){
@@ -7,5 +8,6 @@ function forLoop(array){
       array.push(`I am ${i} strange loops.`);  
     }
   }
+  //send back array
   return array;
 }
